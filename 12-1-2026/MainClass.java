a113baec3d834a3212c0a04991fa57a47adc0b2b
Ahmad-Indoasat <eeeedayat@gmail.com>
2026-01-12 09:27:02 +0700

    Tugas hari ini

---
+++ 12-1-2026/MainClass.java
@@ -0,0 +1,45 @@
+public class MainClass {
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+
+        int angka; // tipe data integer
+        angka = 10;
+        System.out.println("Nomor anda adalah: " + angka);
+        angka = 21;
+        System.out.println("Nomor baru anda adalah: " + angka);
+
+        boolean status; // tipe data boolean
+        status = true;
+        System.out.println("Status anda adalah: " + status);
+        status = false;
+        System.out.println("Status baru anda adalah: " + status);
+
+        double angka2; // tipe data double
+        angka2 = 10.5;
+        System.out.println("Nomor anda adalah: " + angka2);
+        angka2 = 21.5;
+        System.out.println("Nomor baru anda adalah: " + angka2);
+
+        char huruf; // tipe data char
+        huruf = 'A';
+        System.out.println("Huruf anda adalah: " + huruf);
+        huruf = 'B';
+        System.out.println("Huruf baru anda adalah: " + huruf);
+
+        String nama; // tipe data string
+        nama = "Dayy";
+        System.out.println("Nama anda adalah: " + nama);
+        nama = "Dayt";
+        System.out.println("Nama baru anda adalah: " + nama);
+
+        float angka3; // tipe data float
+        angka3 = 10.1293f;
+        System.out.println("Nomor anda adalah: " + angka3);
+        angka3 = 21.32394f;
+        System.out.println("Nomor baru anda adalah: " + angka3);
+
+        final double desimal = 10.12;
+        System.out.println("Final tidak bisa diubah: " + desimal);
+
+    }
+}

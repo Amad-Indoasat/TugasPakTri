d0462f89b4b817a1e1844828fa1123e05a886a1f
Ahmad-Indoasat <eeeedayat@gmail.com>
2026-01-12 08:38:24 +0700

    Tambah java

---
+++ MainClass.java
@@ -0,0 +1,15 @@
+public class MainClass {
+    public static void main(String[] args) {
+        // System.out.print("Vardhan gay");
+        // System.out.println("Vardhan gay 2");
+
+        Float Angka = 0f;
+        // System.out.println(Angka);
+
+        while (Angka < 5) {
+            Angka++;
+            System.out.println("Angka sekarang : " + Angka);
+        }
+
+    }
+}

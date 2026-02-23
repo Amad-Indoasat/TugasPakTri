c2d48c9ff8bf4ac2616e924b9782be935168c468
Amad-Indoasat <eeeedayat@gmail.com>
2026-02-23 09:08:11 +0700

    OOP Dasar

---
+++ 23-2-26/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        notmain objek = new notmain();
+        System.out.println(objek.nama);
+        System.out.println(objek.absen);
+        System.out.println(objek.usia);
+
+    }
+}
---
+++ 23-2-26/notmain.java
@@ -0,0 +1,8 @@
+public class notmain {
+    String nama = "Budi";
+    int absen = 2;
+    int usia = 16;
+
+    // Make OOP
+
+}

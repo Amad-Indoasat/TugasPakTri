9ab3b20ab6818f6a0ea97b0788b64200aeb4db14
Amad-Indoasat <eeeedayat@gmail.com>
2026-03-09 08:37:04 +0700

    Lupa Commit

---
+++ 3-3-26/NotMain.java
@@ -0,0 +1,12 @@
+public class NotMain {
+    String car = "Supra";
+    String bike = "Yamaha";
+
+    void car() {
+        System.out.println("Da Car is " + car);
+    }
+
+    void bike() {
+        System.out.println("Da Bike is " + bike);
+    }
+}

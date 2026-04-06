471acc4f74d28f4fc22dd6b07aed1e35545f5fab
Amad-Indoasat <eeeedayat@gmail.com>
2026-04-06 11:30:45 +0700

    6-April-2026/Pisah Main Class

---
+++ 6-4-26/NotMain.java
@@ -0,0 +1,13 @@
+public class NotMain {
+
+    void perkalian(int a, int b) {
+        System.out.println(a * b);
+    }
+    void perkalian(int a, int b, int c) {
+        System.out.println(a * b * c);
+    }
+
+    void perkalian(double a, double b) {
+        System.out.println(a * b);
+    }
+}

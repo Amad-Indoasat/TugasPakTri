fcc3de808867bea935d2753517c1dd4019a56799
Amad-Indoasat <eeeedayat@gmail.com>
2026-04-14 14:00:56 +0700

    Overloading Again

---
+++ 14-4-26/NotMain.java
@@ -0,0 +1,18 @@
+public class NotMain {
+
+    void perkalian(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    void perkalian(int a, int b, int c) {
+        System.out.println(a * b * c);
+    }
+
+    void perkalian(double a, double b, String c) {
+        System.out.println(c + " = " + a * b);
+    }
+
+    void perkalian(float a, double b) {
+        System.out.println(a * b);
+    }
+}
---
+++ 14-4-26/OverL.java
@@ -0,0 +1,12 @@
+public class OverL {
+    public static void main(String[] args) {
+
+        NotMain nm = new NotMain();
+        nm.perkalian(2, 4);
+        nm.perkalian(9, 8, 10);
+        nm.perkalian(8.5, 4.5, "Hasilnya adalah");
+        nm.perkalian(9.3f, 8.5);
+
+    }
+
+}
